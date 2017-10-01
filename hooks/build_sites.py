bde878baf7e8c106b57ba96d48de7e2fc0c211c8
helrond <helrond@hotmail.com>
2017-09-30 23:34:08 -0400

    have basic copy function working

--- hooks/build_sites.py
+++ hooks/build_sites.py
@@ -4,8 +4,8 @@
 import sys
 import json
 import yaml
-from os import chdir, mkdir
-from os.path import join, isdir
+from os import chdir, mkdir, makedirs
+from os.path import join, isdir, isfile
 from posix import remove
 from shutil import copyfile, copytree, rmtree
 from subprocess import call
@@ -13,33 +13,22 @@ from ConfigParser import ConfigParser, NoSectionError
 
 # use existing config file?
 config = ConfigParser()
-config.read(local_settings.cfg)
+config.read('local_settings.cfg')
 
-with open(sys.argv[1], 'r') as jsf:
-  payload = json.loads(jsf.read())
+# with open(sys.argv[1], 'r') as jsf:
+#   payload = json.loads(jsf.read())
 
-repository_dir = config.get('Directories', 'repositories')
-repository_name = payload['repository']['name']
-repository_url = payload['repository']['ssh_url']
+repository_dir = join(config.get('Directories', 'root'), config.get('Directories', 'repositories'))
+# repository_name = payload['repository']['name']
+repository_name = "test"
+# repository_url = payload['repository']['ssh_url']
 
-# If the repository exists, update the data
-if isdir(join(repository_dir, repository_name)):
-    chdir(join(repository_dir, repository_name))
-    call("git pull")
-    build_sites(repository_name)
-# If the repository doesn't already exist, make a directory and pull down the data
-else:
-    mkdir(join(repository_dir, repository_name))
-    chdir(join(repository_dir, repository_name))
-    call("git pull")
-    call("git clone " + repository_url)
-    build_sites(repository_name)
 
 def build_sites(directory):
     with open('config.yml') as f:
         yaml_config = yaml.load(f)
         # Does it make sense to rely on whether or not the repository is private?
-        if yaml_config['public'] = 'true':
+        if yaml_config['public'] == True:
             sites = [config.get('Directories', 'public'), config.get('Directories', 'private')]
         else:
             sites = [config.get('Directories', 'private')]
@@ -49,25 +38,42 @@ def build_site(name, sites = [], *args):
     for site in sites:
         # We are still in repository/name
         # this file is used to generate the home page
-        copyfile('config.yml', join(site, config.get('Directories', 'staging'), 'data', name + '.yml'))
+        if not isdir(join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), 'data')):
+            makedirs(join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), 'data'))
+        copyfile(join(repository_dir, name, 'config.yml'), join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), 'data', name + '.yml'))
 
-        if isdir(join(site, config.get('Directories', 'staging'), name)):
-            rmtree(join(site, config.get('Directories', 'staging'), name))
-        if isfile(join(site, config.get('Directories', 'staging'), name)):
+        if isdir(join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), name)):
+            rmtree(join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), name))
+        if isfile(join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), name)):
             # in case someone put something (like a softlink) in its place
-            remove(join(site, config.get('Directories', 'staging'), name))
+            remove(join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), name))
 
         try:
-            makedirs(config.get('Directories', 'build'))
+            makedirs(join(config.get('Directories', 'root'), site, config.get('Directories', 'build')))
         except OSError:
             # dir exists
             pass
 
-        copytree(join(repository_dir, name), join(site, config.get('Directories', 'staging'), name))
+        copytree(join(repository_dir, name), join(config.get('Directories', 'root'), site, config.get('Directories', 'staging'), name))
 
-        chdir(site)
+        chdir(join(config.get('Directories', 'root'), site))
 
         #staging --> build
-        call("jekyll build --source %s --destination %s" % config.get('Directories', 'staging'), config.get('Directories', 'build'))
+        # print "jekyll build --source %s --destination %s" % (config.get('Directories', 'staging'), config.get('Directories', 'build'))
+        # call("jekyll build --source %s --destination %s" % (config.get('Directories', 'staging'), config.get('Directories', 'build')))
 
         # set file permissions and ownership if necessary
+
+
+# If the repository exists, update the data
+if isdir(join(repository_dir, repository_name)):
+    chdir(join(repository_dir, repository_name))
+    # call("git pull")
+    build_sites(repository_name)
+# If the repository doesn't already exist, make a directory and pull down the data
+else:
+    mkdir(join(repository_dir, repository_name))
+    chdir(join(repository_dir, repository_name))
+    # call("git pull")
+    # call("git clone " + repository_url)
+    build_sites(repository_name)
