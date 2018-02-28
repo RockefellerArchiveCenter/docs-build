b9251f07383515368cd33f96913a7b1b113c7132
Apache <apache@osticket.dev.rockarch.org>
2018-02-28 13:28:10 -0500

    refactoring for nightly build

---
+++ update.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# pulls files and builds sites
+
+import sys
+import yaml
+from json import loads
+from os import chdir, makedirs, listdir
+from os.path import join, isdir, isfile, normpath, abspath, dirname
+from posix import remove
+from shutil import copyfile, copytree, rmtree
+from subprocess import call
+
+# base path for the build script
+base_path = normpath(abspath(join(dirname(__file__))))
+
+with open(join(base_path, 'config.json'), 'r') as cfg:
+    config = loads(cfg.read())
+
+site_root_dir = config.get('site_root_dir')
+repository_dir = config.get('repository_dir')
+staging_dir = config.get('staging_dir')
+build_dir = config.get('build_dir')
+public_site_dir = config.get('public_site_dir')
+private_site_dir = config.get('private_site_dir')
+
+
+def get_updates(repository_name):
+    # If the repository exists, update the data
+    if isdir(join(site_root_dir, repository_dir, repository_name)):
+        chdir(join(site_root_dir, repository_dir, repository_name))
+        print "pulling from "+repository_name
+        call("git pull", shell=True)
+
+
+def create_structure(target, site, name):
+    if isdir(target):
+        rmtree(target)
+    if isfile(target):
+        # in case someone put something (like a softlink) in its place
+        remove(target)
+    try:
+        makedirs(join(site_root_dir, site, build_dir))
+    except OSError:
+        # dir exists
+        pass
+    copytree(join(site_root_dir, repository_dir, name), target)
+
+
+def build_site(base_url, source, destination):
+    chdir(base_url)
+    print "building at " + base_url
+    print "jekyll build --source %s --destination %s" % (source, destination)
+    call("/usr/local/rvm/gems/ruby-2.1.8/wrappers/jekyll build --source %s --destination %s" % (source, destination), shell=True)
+    # set file permissions and ownership if necessary
+
+
+def build(directory):
+    with open(join(site_root_dir, repository_dir, directory, '_config.yml')) as f:
+        yaml_config = yaml.load(f)
+        if yaml_config['type'] == 'docs':
+            if yaml_config['public'] == True:
+                sites = [public_site_dir, private_site_dir]
+            else:
+                sites = [private_site_dir]
+            update_docs(directory, sites)
+        if yaml_config['type'] == 'theme':
+            update_theme(directory, [public_site_dir, private_site_dir])
+
+
+def update_docs(name, sites=[], *args):
+    print "*** building documentation ***"
+    for site in sites:
+        site_staging_dir = join(site_root_dir, site, staging_dir)
+        # this file is used to generate the site home page
+        if not isdir(join(site_staging_dir, '_data')):
+            makedirs(join(site_staging_dir, '_data'))
+        copyfile(join(site_root_dir, repository_dir, name, '_config.yml'), join(site_root_dir, site_staging_dir, '_data', name + '.yml'))
+
+        create_structure(join(site_staging_dir, name), site, name)
+
+        # build_site(join(site_root_dir, site), staging_dir, build_dir)
+
+
+def update_theme(name, sites=[], *args):
+    print "*** building theme ***"
+    for site in sites:
+        site_staging_dir = join(site_root_dir, site, staging_dir)
+
+        create_structure(site_staging_dir, site, name)
+
+    for s in listdir(join(site_root_dir, repository_dir)):
+        if isdir(join(site_root_dir, repository_dir, s)):
+            with open(join(site_root_dir, repository_dir, s, '_config.yml')) as f:
+                yaml_config = yaml.load(f)
+                if yaml_config['type'] == 'docs':
+                    if yaml_config['public'] == True:
+                        sites = [public_site_dir, private_site_dir]
+                    else:
+                        sites = [private_site_dir]
+                    for site in sites:
+                        copytree(join(site_root_dir, repository_dir, s), join(site_root_dir, site, staging_dir, s))
+
+    # for site in sites:
+    #     build_site(join(site_root_dir, site), staging_dir, build_dir)
+
+def main():
+    for d in listdir(join(site_root_dir, repository_dir)):
+        print d
+        get_updates(d)
+        build(d)
+    build_site(join(site_root_dir, public_site_dir), staging_dir, build_dir)
+    build_site(join(site_root_dir, private_site_dir), staging_dir, build_dir)
+
+main()
