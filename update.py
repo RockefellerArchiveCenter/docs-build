0000a3ddf7a72de249c491308cc7943feab17a8d
Hillel Arnold <helrond@hotmail.com>
2018-04-12 16:22:31 -0400

    debugging

--- update.py
+++ update.py
@@ -79,7 +79,7 @@ def update_docs_structure(name, sites=[], *args):
         with open(data_file) as f:
             yaml_config = yaml.safe_load(f)
         yaml_config['slug'] = name
-        yaml_config['github_repo'] = 'https://github.com/RockefellerArchiveCenter/{}'.format(repository_dir)
+        yaml_config['github_repo'] = 'https://github.com/RockefellerArchiveCenter/{0}'.format(name)
         with open(data_file, 'w') as f:
             yaml.safe_dump(yaml_config, f, default_flow_style=False)
         create_structure(join(site_staging_dir, name), site, name)
