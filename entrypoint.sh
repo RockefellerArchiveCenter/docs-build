ef9fc46a2afb99aed63cdc0324a2e8a9110c7e66
Hillel Arnold <helrond@hotmail.com>
2019-10-30 09:34:06 -0400

    remove hardcoded values

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ cp config.json.sample config.json
 echo "Performing initial build of site"
 python ./update.py
 
-cd /var/www/internal/
+ROOT=$(cat config.json | jq '.site_root' | head -c -2 | tail -c +2)
+SITE=$(cat config.json | jq '.private_site .root' | head -c -2 | tail -c +2)
+BUILD=$(cat config.json | jq '.private_site .build' | head -c -2 | tail -c +2)
+
+cd ${ROOT} && cd ${SITE} && cd ${BUILD}
 
 http-server -p 4000
