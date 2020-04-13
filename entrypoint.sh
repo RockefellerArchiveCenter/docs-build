a07bf97379e17359a215de14965ea8581e35ace0
Hillel Arnold <helrond@hotmail.com>
2020-04-13 13:34:12 -0500

    refactor entrypoint for use in travis ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,19 @@
 echo "Creating config file"
 cp config.json.sample config.json
 
-echo "Performing initial build of site"
-python ./update.py
-
-cd /home/docs/private/build/
-
-http-server -p 4000 . &
-
-cd /home/docs/docs-build/ && inotifywait -e modify,move,create,delete -m theme/ |
-while read -r directory events filename; do
-  echo "Regenerating..."
+if [ -z "$TRAVIS_CI" ]
+then
+  echo "Performing initial build of site"
   python ./update.py
+
+  echo "Starting Apache"
+  /usr/sbin/httpd
+
+  inotifywait -e modify,move,create,delete -m theme/ -r |
+  while read filename; do
+    echo "Regenerating..."
+    python ./update.py
 done
+else
+  python ./update.py
+fi
