13d9f2f2b785e56df1d143686e0a848eda72538f
Hillel Arnold <helrond@hotmail.com>
2021-11-12 16:13:07 -0500

    trying again

--- test_urls.py
+++ test_urls.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from os import environ
 import urllib3
 
 EXPECTED_PATHS = [
@@ -15,6 +16,6 @@ http = urllib3.PoolManager()
 
 for path in EXPECTED_PATHS:
     for port in ["4000", "4001"]:
-        url = "http://localhost:{}{}".format(port, path)
+        url = "http://{}:{}{}".format(environ.get("CONTAINER"), port, path)
         resp = http.request("GET", url)
         assert resp.status == 200, "Did not receive the expected response for {}".format(url)
