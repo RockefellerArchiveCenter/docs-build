50be93d36b859895859ec910c12546758d773722
Hillel Arnold <hillel.arnold@gmail.com>
2023-02-23 15:22:53 -0500

    one step further on deploys

--- scripts/lambda_deploy.sh
+++ scripts/lambda_deploy.sh
@@ -3,27 +3,14 @@
 REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 SOURCE_IMAGE="${DOCKER_REPO}"
 TARGET_IMAGE="${REGISTRY_URL}/${DOCKER_REPO}"
-
-# Construct latest tag
-TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
-
-# Construct version number from datetime and commit hash
 VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 
 # make sure correct region is set
 aws configure set default.region ${AWS_REGION}
-
-# Push image to ECR
-###################
-
 # Log in to AWS
 $(aws ecr get-login --no-include-email)
 
-# update latest version
-docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
-docker push ${TARGET_IMAGE_LATEST}
-
-# push new version
-docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_VERSIONED}
-docker push ${TARGET_IMAGE_VERSIONED}
+aws lambda update-function-code \
+    --function-name  ${LAMBDA_FUNCTION_NAME} \
+    --image-uri ${TARGET_IMAGE_VERSIONED}
