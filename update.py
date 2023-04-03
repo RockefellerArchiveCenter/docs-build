a87cf576c2cbab58fd72b8a9bada02af3f2b407b
Hillel Arnold <hillel.arnold@gmail.com>
2023-04-03 15:23:56 -0400

    remove extra build

--- update.py
+++ update.py
@@ -149,7 +149,6 @@ def main(event=None, context=None):
                 'statusCode': 200,
                 'body': json.dumps(f"Branch {branch} is not eligible to be built")}
 
-        message = UpdateRoutine().run(audience, branch)
         if audience == 'public':
             UpdateRoutine().run('private', branch)
             message = f'Update process for public and private {branch} sites completed at {datetime.now()}'
