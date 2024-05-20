e151ec11d6341d6d9f2a9c0a742508bd1697b455
Hannah <hsistrunk@gmail.com>
2024-05-20 16:14:57 -0400

    remove travis conditional

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,23 @@
 
 # This currently does not work because calling update.py without an event doesn't do anything.
 
-if [ -z "$TRAVIS_CI" ]
-then
-  echo "Performing initial build of site"
-  python ./update.py
 
-  echo "Starting Apache"
-  /usr/sbin/apachectl start
+echo "Performing initial build of site"
+python ./update.py
 
-  echo "
+echo "Starting Apache"
+/usr/sbin/apachectl start
+
+echo "
 Sites built successfully!
-  Public site available at http://localhost:4000
-  Private site available at http://localhost:4001
+Public site available at http://localhost:4000
+Private site available at http://localhost:4001
 "
 
-  inotifywait -e modify,move,create,delete -m theme/ -r |
-  while read filename; do
-    echo "Regenerating..."
-    python ./update.py
-done
-else
+inotifywait -e modify,move,create,delete -m theme/ -r |
+while read filename; do
+  echo "Regenerating..."
   python ./update.py
+done
+
 fi
