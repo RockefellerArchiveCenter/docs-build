15c02654829f2d93cebf760f89429825944f170e
Hannah <hsistrunk@gmail.com>
2024-07-09 09:46:22 -0400

    tweak homepage styles and lint toc id

--- theme/js/toc-toggle.js
+++ theme/js/toc-toggle.js
@@ -3,23 +3,23 @@
   // Side navigation toggle
   $(function () {
     $('#toc-nav-toggle').on('click', function (e) {
-      if ($(this).siblings('#toc__link-group').is(':visible')) {
+      if ($(this).siblings('#toc-link-group').is(':visible')) {
         $(this).html('Open Page Menu')
         $(this).attr('aria-expanded', 'false')
       } else {
         $(this).html('Close Page Menu')
         $(this).attr('aria-expanded', 'true')
       }
-      $(this).siblings('#toc__link-group').toggle()
+      $(this).siblings('#toc-link-group').toggle()
       e.stopPropagation()
     })
     $('html').click(function () {
       if (mobileSmall.matches) {
-        $('#toc__link-group').hide()
-        $('#toc__link-group').siblings('#toc-nav-toggle').html('Open Page Menu')
+        $('#toc-link-group').hide()
+        $('#toc-link-group').siblings('#toc-nav-toggle').html('Open Page Menu')
       } else {
         // allow keyboard focus
-        $('#toc__link-group button').attr('tabindex', '0')
+        $('#toc-link-group button').attr('tabindex', '0')
       }
     })
   })
